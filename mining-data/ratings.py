f3392555f09c11ed2208e4236390f01153433718
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-03-09 19:19:01 +0100

    added f/m user counts

--- mining-data/ratings.py
+++ mining-data/ratings.py
@@ -2,15 +2,29 @@ def readData(filename):
     fs = open('dataset/' + filename + '.dat', 'r')    
     data = [line.strip().split('::') for line in fs]  
     fs.close()                                        
-    return data                                       
-	
+    return data
+    
+def trimLists(users, ratings):
+	# remove occupation and zipcode from users
+    for u in users:
+        del u[-1]    # remove last column: zipcode
+        del u[-1]    # remove last column: occupation
+    
+    # remove timestamps from ratings
+    for r in ratings:
+        del r[-1]    # remove last column: timestamp  
+    
 if __name__ == "__main__":
     # Read data from files
     users   = readData('users')
     movies  = readData('movies')
     ratings = readData('ratings')
+    trimLists(users, ratings)
+        
+    nrFemale = sum(1 for x in users if x[1] == 'F')
+    nrMale   = len(users) - nrFemale
+    print 'Female users: ' + `nrFemale` + '   (' + `len(users)` + ' total users)'
+    print 'Male   users: ' + `nrMale`   + '   (' + `len(users)` + ' total users)'
+        
     
-    # remove occupation and zipcode from users
-    for user in users:
-        del user[-1]    # remove last column: zipcode
-        del user[-1]    # remove last column: occupation
+    
