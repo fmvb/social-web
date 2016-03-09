c0ddad77682c1682ed0d218d3a5a51ccef6fdd0a
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-03-09 18:45:46 +0100

    removed occupation and zipcode from users

--- mining-data/ratings.py
+++ mining-data/ratings.py
@@ -10,3 +10,7 @@ if __name__ == "__main__":
     movies  = readData('movies')
     ratings = readData('ratings')
     
+    # remove occupation and zipcode from users
+    for user in users:
+        del user[-1]    # remove last column: zipcode
+        del user[-1]    # remove last column: occupation
