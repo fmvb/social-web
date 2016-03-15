480fc7bfd1f1fecbd51a62360f53ec631c8273ea
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-03-15 17:36:48 +0100

    read data from staff file and print people with twitter account

--- social-app/python/twitter_connections.py
+++ social-app/python/twitter_connections.py
@@ -1,9 +1,32 @@
 import twitter_auth as tw_auth
+import twitter
+
+def readData(filename):
+    data = []
+    try:
+        fs = open('../' + filename, 'r')
+        try:
+            firstline = fs.readline()
+            for line in fs:
+                name, account, rest = line.strip().split(',', 2)
+                if (name != '' and account !=''):
+                    data.append((name, account))
+        finally:
+            fs.close()
+    except IOError:
+        print 'Error opening file' + filename 
+        return    
+    return data
+
 
 if __name__ == "__main__":
-    twitter_api = tw_auth.twitter_authenticate('twitter_auth_file')
-    if (twitter_api == None):
-        print 'Error in authentication, exiting program.'
-        exit(1)
+    #twitter_api = tw_auth.twitter_authenticate('twitter_auth_file')
+    #if (twitter_api == None):
+        #print 'Error in authentication, exiting program.'
+        #exit(1)
         
+    account_list = readData('tni-staff.csv')
     
+    for name, account in account_list:
+        print name + ': ' + account
+        
